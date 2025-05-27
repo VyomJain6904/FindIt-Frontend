f2401418da80a6ff69dfe1717fcc37c3e935b34b
VyomJain6904 <vyom.malo6904@gmail.com>
2025-05-27 05:36:17 +0000

    Review Cards and Grid Background Created

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { LoginButton } from "@/components/loginButton";
 import { MovingGlobe } from "@/components/movingGlobe";
 import { SearchDomainBox } from "@/components/searchBox";
+import { GridBackground } from "@/components/ui/gridBackground";
 
 export default function Home() {
 	return (
@@ -9,6 +10,7 @@ export default function Home() {
 			<div className="flex flex-col items-center justify-center min-h-screen space-y-8 px-4">
 				<MovingGlobe />
 				<SearchDomainBox />
+				<GridBackground />
 			</div>
 		</div>
 	);
--- frontend/src/components/searchBox.tsx
+++ frontend/src/components/searchBox.tsx
@@ -31,8 +31,6 @@ export function SearchDomainBox() {
 					Search &rarr;
 					<BottomGradient />
 				</button>
-
-				<div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
 			</form>
 		</div>
 	);

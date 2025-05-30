f04d59a621e498555ccdec7dc2eb3270a47090b8
VyomJain6904 <vyom.malo6904@gmail.com>
2025-05-30 06:35:14 +0000

    File Upload Component added

---
+++ frontend/src/components/searchPageUI/custom-file-upload.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React, { useState } from "react";
+import { FileUpload } from "@/components/ui/file-upload";
+
+export function CustomFileUpload() {
+	const [files, setFiles] = useState<File[]>([]);
+	const handleFileUpload = (files: File[]) => {
+		setFiles(files);
+		console.log(files);
+	};
+
+	return (
+		<div className="w-full max-w-4xl mx-auto min-h-96 bg-black rounded-lg">
+			<FileUpload onChange={handleFileUpload} />
+		</div>
+	);
+}
--- frontend/src/components/searchPageUI/searchSideBar.tsx
+++ frontend/src/components/searchPageUI/searchSideBar.tsx
@@ -8,6 +8,7 @@ import { SearchButton } from "@/components/searchPageUI/searchButton";
 import ColourfulText from "@/components/ui/colourful-text";
 import { SearchDomainBox } from "@/components/searchPageUI/searchBox";
 import { cn } from "@/lib/utils";
+import { CustomFileUpload } from "@/components/searchPageUI/custom-file-upload";
 
 import {
 	IconArrowLeft,
@@ -119,10 +120,10 @@ export const LogoIcon = () => {
 
 const Dashboard = () => {
 	return (
-		<div className="items-center justify-center h-full w-full">
-			<div className="flex flex-row items-center justify-center py-10 h-screen md:h-auto dark:bg-black bg-white relative w-full">
+		<div className="items-center justify-center h-full w-full ">
+			<div className="flex flex-row items-center justify-center py-10 h-screen md:h-auto bg-black relative w-full">
 				<div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
-					<h2 className="text-center text-3xl md:text-5xl font-bold text-black dark:text-white">
+					<h2 className="text-center text-3xl md:text-5xl font-bold text-white">
 						<ColourfulText text="FindIt" />
 					</h2>
 					<p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
@@ -132,6 +133,7 @@ const Dashboard = () => {
 					<div className="mt-5">
 						<SearchDomainBox />
 						<SearchButton />
+						<CustomFileUpload />
 					</div>
 				</div>
 			</div>
